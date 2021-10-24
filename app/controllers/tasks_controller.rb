2fae463f6524c3a291af6c0d096d168b3d58494b
shiori0221 <sarasa8221@gmail.com>
2021-10-24 16:17:20 +0900

    コントローラーの設定

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,24 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.list_id = "1"
+    if @task.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:text, :memo, :limit, :list_id)
+  end
+
 end
